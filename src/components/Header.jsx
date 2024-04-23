c8b820403db36c51b602a984b791cc3fac6551ef
OsborneLinus <linusholm1997@gmail.com>
2024-04-23 10:13:24 +0200

    Made the header clickable and return to the /

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import headerImg from "../assets/header.svg";
 export default function Header() {
   return (
     <div className="header">
-      <img src={headerImg}></img>
+      <a href="/">
+        <img src={headerImg}></img>
+      </a>
     </div>
   );
 }
