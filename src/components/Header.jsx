f71f9a5237373737a8758a5f9d434583af561033
OsborneLinus <linusholm1997@gmail.com>
2024-04-23 10:35:28 +0200

    Added className to <a> to make sure that the headerImg i mobile fit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import headerImg from "../assets/header.svg";
 export default function Header() {
   return (
     <div className="header">
-      <a href="/">
+      <a className="header" href="/">
         <img src={headerImg}></img>
       </a>
     </div>
